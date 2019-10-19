53576659cbd7396f99c008514d45d626cad926d0
Perrin Brown <macadmin@Perrins-Macbook-Pro.local>
2019-10-19 16:12:17 -0400

    mvp

--- app.js
+++ app.js
@@ -1,88 +1,140 @@
 $(() => {
+  // let selectedRuntime = 0;
+  // let selectedStatus = "";
+  // let selectedType = "";
+
+  // const findRuntimes = data => {
+  //   for (let i = 0; i < data.length; i++) {
+  //     const runtime = data[i].runtime;
+
+  //     if (runtime == selectedRuntime) {
+  //       const $name = $("<li>").text(data[i].name);
+  //       $("body").append($name);
+  //       $name.attr("class", "names");
+  //       $name.attr("id", "names");
+  //     }
+  //   }
+  // };
+
+  // const findStatus = data => {
+  //   for (let i = 0; i < data.length; i++) {
+  //     const status = data[i].status;
+
+  //     if (status == selectedStatus) {
+  //       const $name = $("<ol>").text(data[i].name);
+  //       $("body").append($name);
+  //       $name.attr("class", "names");
+  //       $name.attr("id", "status");
+  //     }
+  //   }
+  // };
+
+  // const findType = data => {
+  //   for (let i = 0; i < data.length; i++) {
+  //     const type = data[i].type;
+
+  //     if (type == selectedType) {
+  //       const $name = $("<h2>").text(data[i].name);
+  //       $("body").append($name);
+  //       $name.attr("class", "names");
+  //       $name.attr("id", "type");
+  //     }
+  //   }
+  // };
+
+  // $("select").on("change", function() {
+  //   // selectedRuntime = this.value;
+
+  //   // findRuntimes(data);
+
+  //   // selectedStatus = this.value;
+  //   // findStatus(data);
+  //   $(".names").remove();
+  //   selectedGenre = this.value;
+  //   findGenre(data);
+
+  //   // selectedType = this.value;
+  //   // findType(data);
+  // });
   const showList = `http://api.tvmaze.com/shows`;
 
   $.ajax({ url: showList, async: true }).then(data => {
     data = data;
-    console.log(data);
+    console.log(data[0]);
 
-    let selectedRuntime = 0;
-    let selectedStatus = "";
     let selectedGenre = "";
-    let selectedType = "";
-
-    const findRuntimes = data => {
-      for (let i = 0; i < data.length; i++) {
-        const runtime = data[i].runtime;
-
-        if (runtime == selectedRuntime) {
-          const $name = $("<li>").text(data[i].name);
-          $("body").append($name);
-          $name.attr("class", "names");
-          $name.attr("id", "names");
-        }
-      }
-    };
-
-    const findStatus = data => {
-      for (let i = 0; i < data.length; i++) {
-        const status = data[i].status;
-
-        if (status == selectedStatus) {
-          const $name = $("<ol>").text(data[i].name);
-          $("body").append($name);
-          $name.attr("class", "names");
-          $name.attr("id", "status");
-        }
-      }
-    };
-
-    const findType = data => {
-      for (let i = 0; i < data.length; i++) {
-        const type = data[i].type;
-
-        if (type == selectedType) {
-          const $name = $("<h2>").text(data[i].name);
-          $("body").append($name);
-          $name.attr("class", "names");
-          $name.attr("id", "type");
-        }
-      }
-    };
-
     const findGenre = data => {
       for (let i = 0; i < data.length; i++) {
         for (let j = 0; j < data[i].genres.length; j++) {
           const genre = data[i].genres[j];
 
           if (genre == selectedGenre) {
-            const $name = $("<h3>").text(data[i].name);
-            $("body").append($name);
+            const $name = $("<h3>").text("Show Title: " + data[i].name);
             $name.attr("class", "names");
-            $name.attr("id", "genre");
 
-            if (genre == selectedGenre) {
-              const $img = $("<img>").attr("src", data[i].image.medium);
-              $("body").append($img);
-              console.log($img);
-            }
+            const $img = $("<img>").attr("src", data[i].image.medium);
+            $img.attr("class", "names");
+
+            const $runtime = $("<p>").text(
+              "Runtime: " + data[i].runtime + " Minutes"
+            );
+            $runtime.attr("class", "names");
+
+            const $premieredate = $("<p>").text(
+              "Premiered: " + data[i].premiered + " on " + data[i].network.name
+            );
+            $runtime.attr("class", "names");
+
+            const $rating = $("<p>").text(
+              "Average Rating: " + data[i].rating.average
+            );
+            $runtime.attr("class", "names");
+
+            const $summary = $("<div>").html(data[i].summary);
+            $summary.attr("class", "names");
+            $summary.attr("id", "summary");
+
+            const $grandparentdiv = $("<div>");
+            $grandparentdiv.attr("class", "names");
+            $grandparentdiv.attr("id", "grandparentdiv");
+            $("body").append($grandparentdiv);
+            $grandparentdiv.append($img);
+
+            const $parentdiv = $("<div>");
+            $parentdiv.attr("class", "names");
+            $parentdiv.attr("id", "parentdiv");
+            $grandparentdiv.append($parentdiv);
+            $parentdiv.append($name);
+            $parentdiv.append($runtime);
+            $parentdiv.append($premieredate);
+            $parentdiv.append($rating);
+            $parentdiv.append($summary);
           }
         }
       }
     };
 
-    $("select").on("change", function() {
-      selectedRuntime = this.value;
+    window.onscroll = function() {
+      stickyNav();
+    };
+
+    let buttonBar = document.getElementById("button-row");
+    let sticky = buttonBar.offsetTop;
+
+    function stickyNav() {
+      if (window.pageYOffset >= sticky) {
+        buttonBar.classList.add("sticky");
+      } else {
+        buttonBar.classList.remove("sticky");
+      }
+    }
+
+    $("button").on("click", event => {
+      event.preventDefault();
       $(".names").remove();
-      findRuntimes(data);
-
-      selectedStatus = this.value;
-      findStatus(data);
-
-      selectedGenre = this.value;
+      selectedGenre = $(event.currentTarget).val();
+      console.log(selectedGenre);
       findGenre(data);
-
-      selectedType = this.value;
-      findType(data);
     });
   });
 });
